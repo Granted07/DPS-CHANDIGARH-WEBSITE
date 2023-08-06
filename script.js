8e0c61e11076b2fc710709e1fc6de06b43d1b7f0
Granted07 <mail>
2023-08-06 19:40:58 +0530

    second commit (bad)

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.getElementsByClassName('button-extend').onclick = function(){location.href = 'google.com'}
+
+function homePage() {
+    document.getElementById("main")
+        .innerHTML += 
+            "<div id='home' class='column'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil similique aperiam commodi. Dignissimos maiores, illum at voluptatum cum odit voluptates necessitatibus asperiores mollitia quo recusandae, distinctio exercitationem doloremque vero doloribus.</p></div>";
+}
