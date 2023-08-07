797ac7386f5aafa52dabed9cdfb346bff11ff52b
Granted07 <mail>
2023-08-07 09:50:49 +0530

    3rd commit (worse)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 function homePage() {
-    document.getElementById("main")
-        .innerHTML += 
-            "<div id='home' class='column'><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil similique aperiam commodi. Dignissimos maiores, illum at voluptatum cum odit voluptates necessitatibus asperiores mollitia quo recusandae, distinctio exercitationem doloremque vero doloribus.</p></div>";
+    document.getElementById("main").removeAttribute('overflow');
+    document.getElementById("main").style.overflowY = "auto";
 }
